717f34b937bb04bac589819bcff9fdfcd07a1c6c
Meshack Tshenolo Koontse <meshackkoontse9@gmail.com>
2023-12-14 14:42:30 +0200

    adding meta tags and titles

--- meshack-t-koontse/src/pages/index.js
+++ meshack-t-koontse/src/pages/index.js
@@ -2,10 +2,19 @@ import LandingPage from "../components/LandingPage";
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
 import ContactForm from "../components/ContactForm";
+import Head from "next/head";
 
 const HomePage = () => {
   return (
     <>
+      <Head>
+        <title>Meshack Tshenolo Koontse</title>
+        <meta name="description" content="Full-stack developer in Botswana" />
+        <meta
+          name="google-site-verification"
+          content="ow_WhINdUJ7A1uoK2iFPiR8gOO-aRarrPmhCD7uQVTk"
+        />
+      </Head>
       <LandingPage />
       <AboutMe />
       <Projects />
